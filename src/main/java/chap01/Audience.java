c6334a80e277eb19dac7783816f7b581260bd91f
owl-cookie <chulwoon.jang@gmail.com>
2019-09-06 14:34:48 +0900

    init

---
+++ src/main/java/chap01/Audience.java
@@ -0,0 +1,16 @@
+package chap01;
+
+public class Audience {
+
+	private Bag bag;
+
+	public Audience(Bag bag) {
+		this.bag = bag;
+	}
+
+	public Bag getBag() {
+		return bag;
+	}
+
+
+}
---
+++ src/main/java/chap01/Invitation.java
@@ -0,0 +1,7 @@
+package chap01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+	private LocalDateTime when;
+}
